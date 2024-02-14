6eb1b58aa4c5357c09d0e819d80c531f1d9d66a3
Kim Robinson <kimmykokonut@gmail.com>
2024-02-13 21:40:51 -0800

    create api call to display on homepage

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,30 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using MvcApiCall.Models;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 
 namespace MvcApiCall.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    private readonly string _apikey;
+    public HomeController(IConfiguration configuration)
     {
-        _logger = logger;
+        _apikey = configuration["NYT"];
     }
+    // private readonly ILogger<HomeController> _logger;
+
+    // public HomeController(ILogger<HomeController> logger)
+    // {
+    //     _logger = logger;
+    // }
 
     public IActionResult Index()
     {
-        return View();
+        List<Article> allArticles = Article.GetArticles(_apikey);
+        return View(allArticles);
     }
 
     public IActionResult Privacy()
